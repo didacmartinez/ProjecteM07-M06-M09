5d3b7eac4965029910751f9aedb5696660aba17c
didacmartinez <dimasa@fp.insjoaquimmir.cat>
2023-10-20 16:23:03 +0200

    Codi base del projecte

---
+++ laravel/config/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return [
+   'name'      => env('ADMIN_NAME', 'daw2_15'),
+   'email'     => env('ADMIN_EMAIL', '2daw.equip15@fp.insjoaquimmir.cat'),
+   'password'  => env('ADMIN_PASSWORD', '4RrBF46/63.:'),
+];
