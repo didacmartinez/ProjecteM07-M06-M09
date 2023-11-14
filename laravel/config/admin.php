b2c93e9edd6860aa35ab057bb13b7c747a6a1b4f
didacmartinez <dimasa@fp.insjoaquimmir.cat>
2023-11-14 21:00:32 +0100

    Remove OLD laravel

--- laravel/config/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-return [
-   'name'      => env('ADMIN_NAME', 'daw2_15'),
-   'email'     => env('ADMIN_EMAIL', '2daw.equip15@fp.insjoaquimmir.cat'),
-   'password'  => env('ADMIN_PASSWORD', '4RrBF46/63.:'),
-];
