018da7a16aeff8077791e71a32ea743fbfa8c802
Didac <130497562+didacmartinez@users.noreply.github.com>
2023-11-29 00:39:08 +0100

    index.blade punto 2 hecha

--- laravel/resources/views/posts/index.blade.php
+++ laravel/resources/views/posts/index.blade.php
@@ -11,7 +11,7 @@
         "file_id",
         "latitude",
         "longitude",
-        "likes_count", // Agrega la columna para el contador de likes
+        "likes_count",
         "created_at",
         "updated_at"
     ];
